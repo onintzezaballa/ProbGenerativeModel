1316fc14efca3d8e4044a1bb6925a337bf5fc375
Onintze Zaballa <62137318+onintzezaballa@users.noreply.github.com>
2022-01-27 16:53:12 +0100

    Add files via upload

--- Evaluation/mainEvaluation.R
+++ Evaluation/mainEvaluation.R
@@ -1,15 +1,15 @@
-
-library(MCMCpack)
-library(ggplot2)
+require(ggplot2)
+require(MCMCpack)
 source("github/Evaluation/evaluation_functions.R")
 
+#### PARAMETERS ####
 N <- c(300, 400, 500, 600, 700, 800, 900, 1000, 2000, 3000)
 num.classes <- 3
-max.stages <-4
-min.stages <-3
+max.stages <- 4
+min.stages <- 3
 actions <- c( "A", "B", "C", "D", "E", "G", "H", "I", "J", "K")
-seeds <- c(2,5,8,10,12)
-
+seeds <- c(2, 5, 8, 10, 12)
+#########
 
 data <- data.frame('Seed'= 0,
                    'N.Sec'= 0,
@@ -21,97 +21,50 @@ data <- data.frame('Seed'= 0,
                    'll_real_train'= 0,
                    'll_real_test'= 0)
 
-A_S <- paste0(rep(actions,times=max.stages),rep(1:max.stages, each = length(actions))) 
-A_S <- c(A_S,paste0("END",max.stages))
 
 for (seed in seeds){
   n<- N[1]
-  data.directory <- paste0("data/",n,"_",num.classes,
+  data.directory <- paste0("HIPATIA/Model 3 dynamic programming synthetic data/results/",n,"_",num.classes,
                            "classes_",max.stages,"stages_",min.stages,"minstages_",seed)   # Import the original model
   
-  original_MarkovModel <- list()
-  original_initialization <- list()
-  for (ci in 1:num.classes){
-    model <- 1
-    file <- paste0(data.directory,"/real_class",ci,"_MarkovModel",model,".csv")
-    MarkovModel1 <- read.csv(file, header = F, sep = "," )
-    rnames <- MarkovModel1[2:(nrow(MarkovModel1)),1]
-    cnames <- as.character(as.matrix(MarkovModel1[1,1:(ncol(MarkovModel1)-1)]))
-    MarkovModel1 <- MarkovModel1[-1,-1]
-    MarkovModel1 <- sapply(MarkovModel1, function(x) {return(as.numeric(as.character(x)))})
-    rownames(MarkovModel1) <- rnames
-    colnames(MarkovModel1) <- cnames
-    MarkovModel1 <- as.data.frame(MarkovModel1)
-    original_MarkovModel[[ci]] <- list(MarkovModel1) # vamos a tener un MM por clase
-    
-    model <- 2
-    file <- paste0(data.directory,"/real_class",ci,"_MarkovModel",model,".csv")
-    MarkovModel2 <- read.csv(file, header = F, sep = "," )
-    rnames <- MarkovModel2[2:(nrow(MarkovModel2)),1]
-    cnames <- as.character(as.matrix(MarkovModel2[1,1:(ncol(MarkovModel2)-1)]))
-    MarkovModel2 <- MarkovModel2[-1,-1]
-    MarkovModel2 <- sapply(MarkovModel2, function(x) {return(as.numeric(as.character(x)))})
-    rownames(MarkovModel2) <- rnames
-    colnames(MarkovModel2) <- cnames
-    MarkovModel2 <- as.data.frame(MarkovModel2)
-    original_MarkovModel[[ci]][[2]] <- MarkovModel2 
-    file <- paste0(data.directory,"/real_class",ci,"_initialization.csv")
-    original_initialization[[ci]] <- read.csv(file, header = T, sep = "," )
-  }
+  #data.directory <- paste0("data/",n,"_",num.classes,
+  #                         "classes_",max.stages,"stages_",min.stages,"minstages_",seed)   # Import the original model
   
-  file <- paste0(data.directory,"/real_probability_classes.csv")
-  original_theta.c <- read.csv(file, header = T, sep = "," )
+  # Original model:
+  original_MarkovModel <- lapply(1:num.classes, function(ci){file1 <- paste0(data.directory,"/real_class",ci,"_MarkovModel1.csv"); file2 <- paste0(data.directory,"/real_class",ci,"_MarkovModel2.csv") 
+    MM <- list(as.data.frame(read.csv(file1, header = T, sep = "," )), as.data.frame(read.csv(file2, header = T, sep = "," ) ));
+    colnames(MM[[2]]) <- 1:max.stages;
+    MM} )
+  original_initialization <- lapply(1:num.classes, function(ci){ file <- paste0(data.directory,"/real_class",ci,"_initialization.csv");
+    as.data.frame(read.csv(file, header = T, sep = ","))})
+  
+  original_theta.c <- read.csv(paste0(data.directory,"/real_probability_classes.csv"), header = T, sep = "," )
 
-  test <- Test_Generator(seed, N.total =4, original_theta.c, original_MarkovModel, original_initialization) # 4000 sequences for test dataset
+  test <- Test_Generator(seed, N.total=4000, original_theta.c, original_MarkovModel, original_initialization) # 4000 sequences for test dataset
   ll_real_test <- ll_a(test, num.classes, max.stages, original_theta.c, original_MarkovModel, original_initialization, min.stages) # evaluate the test dataset in the original model
   
+
   for (n in N){
-    data.directory <- paste0("data/",n,"_",num.classes,"classes_",max.stages,"stages_",min.stages,"minstages_",seed) 
-    train <- read.csv(paste0(data.directory,"/data.csv")) # train data 
-    MarkovModel <- list()
-    initialization <- list()
-    for (ci in 1:num.classes){
-      model <- 1
-      file <- paste0(data.directory,"/class",ci,"_MarkovModel",model,".csv")
-      MarkovModel1 <- read.csv(file, header = F, sep = "," )
-      rnames <- MarkovModel1[2:(nrow(MarkovModel1)),1]
-      cnames <- as.character(as.matrix(MarkovModel1[1,1:(ncol(MarkovModel1)-1)]))
-      MarkovModel1 <- MarkovModel1[-1,-1]
-      MarkovModel1 <- sapply(MarkovModel1, function(x) {return(as.numeric(as.character(x)))})
-      rownames(MarkovModel1) <- rnames
-      colnames(MarkovModel1) <- cnames
-      MarkovModel1 <- as.data.frame(MarkovModel1)
-      MarkovModel[[ci]] <- list(MarkovModel1)
-      
-      model <- 2
-      file <- paste0(data.directory,"/class",ci,"_MarkovModel",model,".csv")
-      MarkovModel2 <- read.csv(file, header = F, sep = "," )
-      rnames <- MarkovModel2[2:(nrow(MarkovModel2)),1]
-      cnames <- as.character(as.matrix(MarkovModel2[1,1:(ncol(MarkovModel2)-1)]))
-      MarkovModel2 <- MarkovModel2[-1,-1]
-      MarkovModel2 <- sapply(MarkovModel2, function(x) {return(as.numeric(as.character(x)))})
-      rownames(MarkovModel2) <- rnames
-      colnames(MarkovModel2) <- cnames
-      MarkovModel2 <- as.data.frame(MarkovModel2)
-      MarkovModel[[ci]][[2]] <- MarkovModel2 
-      file <- paste0(data.directory,"/class",ci,"_initialization.csv")
-      initialization[[ci]] <- read.csv(file, header = T, sep = "," )
-    }
+    data.directory <- paste0("HIPATIA/Model 3 dynamic programming synthetic data/results/",n,"_",num.classes,
+                             "classes_",max.stages,"stages_",min.stages,"minstages_",seed)   # Import the original model
+    # data.directory <- paste0("data/",n,"_",num.classes,
+    #                           "classes_",max.stages,"stages_",min.stages,"minstages_",seed) 
+
+    train <- read.csv(paste0(data.directory,"/data.csv")) # train data
     
-    file <- paste0(data.directory,"/probability_classes.csv")
-    theta_c <- read.csv(file, header = T, sep = "," )
-    theta_c <- t(theta_c)
+    # Learned model:
+    learned_MarkovModel <- lapply(1:num.classes, function(ci){file1 <- paste0(data.directory,"/class",ci,"_MarkovModel1.csv"); file2 <- paste0(data.directory,"/class",ci,"_MarkovModel2.csv") 
+    MM <- list(as.data.frame(read.csv(file1, header = T, sep = "," )), as.data.frame(read.csv(file2, header = T, sep = "," ) ));
+    colnames(MM[[2]]) <- 1:max.stages;
+    MM} )
+    learned_initialization <- lapply(1:num.classes, function(ci){ file <- paste0(data.directory,"/class",ci,"_initialization.csv");
+    as.data.frame(read.csv(file, header = T, sep = ","))})
+    learned_theta.c <- t(read.csv(paste0(data.directory,"/probability_classes.csv"), header = T, sep = "," ))
     
-    learned_MarkovModel <- MarkovModel
-    learned_initialization <- initialization
-    learned_theta.c <- theta_c
     
-    ll_train_train <- ll_a(train, num.classes, max.stages, learned_theta.c, learned_MarkovModel, 
-                           learned_initialization, min.stages) # train dataset on the learned model
-    ll_real_train <- ll_a(train, num.classes, max.stages, original_theta.c, original_MarkovModel, 
-                          original_initialization, min.stages) # train dataset on the original model
-    ll_train_test <- ll_a(test, num.classes, max.stages, learned_theta.c, learned_MarkovModel, 
-                          learned_initialization, min.stages) # test dataset on the learned model
+    ll_train_train <- ll_a(train, num.classes, max.stages, learned_theta.c, learned_MarkovModel, learned_initialization, min.stages) # train dataset on the learned model
+    ll_real_train <- ll_a(train, num.classes, max.stages, original_theta.c, original_MarkovModel, original_initialization, min.stages) # train dataset on the original model
+    ll_train_test <- ll_a(test, num.classes, max.stages, learned_theta.c, learned_MarkovModel, learned_initialization, min.stages) # test dataset on the learned model
     
     
     data_n <- data.frame('Seed'=seed,
@@ -155,6 +108,7 @@ data1$Value[1:nrow(data.mean)] <- data.mean$ll_train_train
 data1$Value[(nrow(data.mean)+1): (2*nrow(data.mean))] <- data.mean$ll_train_test
 data1$Value[(2*nrow(data.mean)+1): (3*nrow(data.mean))] <- data.mean$ll_real_train
 data1$Value[(3*nrow(data.mean)+1): (4*nrow(data.mean))] <- data.mean$ll_real_test
+
 
 data1$int <- paste(data1$Data, data1$Model, sep=".")
 (plot <-ggplot(data1, aes(x=Number.Sec, y=Value, colour = int, linetype=int, shape = int, size = int)) + theme_bw()+
